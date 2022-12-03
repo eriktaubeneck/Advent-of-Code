cfefce7909109f8076f047ab7b01d8c2138ec6b7
Erik Taubeneck <erik.taubeneck@gmail.com>
2022-12-03 08:53:33 -0800

    day 1 part 2

--- 2022/day1/day1.py
+++ 2022/day1/day1.py
@@ -2,7 +2,7 @@
 elves = []
 accumulator = 0
 
-with open('part1-input.txt', 'rb') as f:
+with open('input.txt', 'rb') as f:
     for value in f:
         if value == b'\n':
             elves.append(accumulator)
@@ -10,4 +10,20 @@ with open('part1-input.txt', 'rb') as f:
         else:
             accumulator += int(value.decode('utf8').strip('\n'))
 
-print(max(elves))
+elves.append(accumulator)
+
+print(elves[-1])
+
+max_calories = max(elves)
+
+print(f'part 1, max calories: {max_calories}')
+
+second_max_calories = max(v for v in elves if v not in [max_calories])
+third_max_calories = max(
+    v for v in elves if v not in [max_calories, second_max_calories]
+)
+
+print(
+    f'part 2, max 3 calories: '
+    f'{max_calories + second_max_calories + third_max_calories}'
+)
